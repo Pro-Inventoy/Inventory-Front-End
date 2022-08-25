ac05302986d748e1ae25ca582703ff952e039b77
Khayman King <khaymanak@gmail.com>
2022-08-25 14:29:25 -0700

    more componenets added attempted for auth to work

--- Inventory-Front-End/src/components/NavBar/NavBar.jsx
+++ Inventory-Front-End/src/components/NavBar/NavBar.jsx
@@ -1,21 +1,21 @@
 import React from 'react'
 import { IconButton, GroupsIcon, AccountBoxIcon, WarehouseIcon, PendingActionsIcon, HomeIcon } from '@mui/material'
-import style from './NavBar.css'
+import styles from './NavBar.css'
 
 export default function NavBar() {
   return (
-    <div className={style.top-nav}>
-        <IconButton color="primary" aria-label="upload picture" component="label">
-             Home<HomeIcon />
+    <div className={styles.nav}>
+     <IconButton color="primary" aria-label="upload picture" component="label">
+          Home<HomeIcon />
+     </IconButton>
+     <IconButton color="primary" aria-label="upload picture" component="label">
+          Orders<PendingActionsIcon />
+     </IconButton>
+     <IconButton color="primary" aria-label="upload picture" component="label">
+          Inventory<WarehouseIcon />
         </IconButton>
         <IconButton color="primary" aria-label="upload picture" component="label">
-             Orders<PendingActionsIcon />
-        </IconButton>
-        <IconButton color="primary" aria-label="upload picture" component="label">
-             Inventory<WarehouseIcon />
-        </IconButton>
-        <IconButton color="primary" aria-label="upload picture" component="label">
-             Users<GroupsIcon />
+          Users<GroupsIcon />
         </IconButton>
         <IconButton color="primary" aria-label="upload picture" component="label">
              Profile<AccountBoxIcon />
