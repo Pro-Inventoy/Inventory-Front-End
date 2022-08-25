b0c2a2b0543f5c7f2e00055138e129c0857ab8f2
THOMAS HARDING <hardingt604@gmail.com>
2022-08-25 18:59:23 -0500

    join statement with categories table instead of using plain category id

--- Inventory-Front-End/src/components/Inventory/InventoryItem.jsx
+++ Inventory-Front-End/src/components/Inventory/InventoryItem.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import './InventoryItem.css';
 
 export default function InventoryItem({item}) {
   return (
-    <li>
-      {item.inventoryId} {item.quantity} {item.itemname} {item.categoryId} {item.cost}
+    <li className='item'>
+      <div>
+        Quantity: {item.quantity}
+      </div>
+      <div>
+        Item: {item.itemname}
+      </div>
+      <div>
+        Category: {item.Categories.category_name}
+      </div>
+      <div>
+        Total Cost:${item.cost * item.quantity}
+      </div> 
     </li>
   )
 }
