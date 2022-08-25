25c10c5a83edca8d8493a5030a169cf3d6cb9cde
Khayman King <khaymanak@gmail.com>
2022-08-25 10:18:37 -0700

    we have paths

--- Inventory-Front-End/src/components/App.jsx
+++ Inventory-Front-End/src/components/App.jsx
@@ -1,14 +1,26 @@
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import React from 'react'
-import Routes from "./Routes";
+import Homepage from './Homepage/Homepage.jsx'
+import Inventory from './Inventory/Inventory.jsx'
+import Orders from './Orders/Orders.jsx'
+import Users from './Users/Users.jsx'
+import Profile from './Profile/Profile.jsx'
 
 export default function App() {
   return (
     <div>
-      word
-        {/* <Router>
-            <Routes/>
-        </Router> */}
+        <Router>
+            <Routes>
+                {/* <Route element={<AuthPage />} />
+                <Route element={<Layout />}> */}
+                    <Route index element={<Homepage />} />
+                    <Route path="inventory" element={<Inventory />} />
+                    <Route path="orders" element={<Orders />} />
+                    <Route path="users" element={<Users />} />
+                    <Route path="profile" element={<Profile />} />
+                {/* </Route> */}
+            </Routes>
+        </Router>
     </div>
   )
 }
